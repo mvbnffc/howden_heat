20bbda92c681fad2b4e51c403a917dbd533c23d2
Mark Bernhofen <markbernhofen@icloud.com>
2026-03-06 09:56:17 +0000

    add heat exposure response function

--- scripts/productivity.py
+++ scripts/productivity.py
@@ -16,8 +16,8 @@ import numpy as np
 # Example shape: [23, 24, 25, ..., 39] -> [1.0, 0.9, 0.8, ..., 0.0]
 # ---------------------------------------------------------------------------
 
-WBGT_THRESHOLDS = None       # TODO: replace with list of WBGT breakpoints (°C)
-PRODUCTIVITY_VALUES = None   # TODO: replace with corresponding productivity fractions
+WBGT_THRESHOLDS = [26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39] # list of WBGT breakpoints (°C)
+PRODUCTIVITY_VALUES = [1.0, 0.923, 0.846, 0.769, 0.692, 0.615, 0.539, 0.462, 0.385, 0.308, 0.231, 0.154, 0.077, 0.0]   # corresponding productivity fractions
 
 
 def wbgt_to_productivity(wbgt_array: np.ndarray) -> np.ndarray:
